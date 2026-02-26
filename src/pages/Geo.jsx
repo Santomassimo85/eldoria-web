042739c2c657aa257f245f1cf88e193e15adcb06
Santomassimo85 <Santomassimo85@gmail.com>
2026-02-26 17:25:42 +0100

    geo fixed 2 column

--- src/pages/Geo.jsx
+++ src/pages/Geo.jsx
@@ -19,37 +19,37 @@ export default function Geo() {
   if (loading) return <p>Consultando le mappe antiche...</p>;
 
   return (
-    <section>
-      <h1>Archivio Geomantico</h1>
-      <section className="city">
-        {locations.map((loc) => (
-          <ToggleSection key={loc.id} title={loc.name} defaultOpen={false}>
-            <img src={loc.image} alt={loc.name} className="city-img" />
-            <br />
-            
-            <div 
-              className="geo-description" 
-              dangerouslySetInnerHTML={{ __html: loc.description }} 
-            />
-            <br />
-            
-            {/* Conditional POI Section */}
-            {loc.pointsOfInterest && loc.pointsOfInterest.length > 0 && (
-              <>
-                <h4 style={{ textAlign: "left" }}>Punti di Interesse</h4>
-                <ul style={{ padding: 0 }}>
-                  {loc.pointsOfInterest.map((poi, index) => (
-                    <li key={index} style={{ display: "flex", alignItems: "center", marginBottom: "10px", gap: "10px" }}>
-                      <img src={poi.icon} alt="icon" style={{ width: "24px", height: "24px" }} />
-                      <span>{poi.label}</span>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-          </ToggleSection>
-        ))}
-      </section>
+  <section>
+    <h1>Archivio Geomantico</h1>
+    {/* Il container .city ora gestisce la griglia CSS */}
+    <section className="city">
+      {locations.map((loc) => (
+        <ToggleSection key={loc.id} title={loc.name} defaultOpen={false}>
+          <img src={loc.image} alt={loc.name} className="city-img" />
+          <br />
+          
+          <div 
+            className="geo-description" 
+            dangerouslySetInnerHTML={{ __html: loc.description }} 
+          />
+          
+          {/* La sezione POI rimane condizionale come richiesto */}
+          {loc.pointsOfInterest && loc.pointsOfInterest.length > 0 && (
+            <div className="poi-section">
+              <h4 style={{ textAlign: "left" }}>Punti di Interesse</h4>
+              <ul style={{ padding: 0 }}>
+                {loc.pointsOfInterest.map((poi, index) => (
+                  <li key={index} style={{ display: "flex", alignItems: "center", marginBottom: "10px", gap: "10px" }}>
+                    <img src={poi.icon} alt="icon" style={{ width: "24px", height: "24px" }} />
+                    <span>{poi.label}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </ToggleSection>
+      ))}
     </section>
-  );
+  </section>
+);
 }
